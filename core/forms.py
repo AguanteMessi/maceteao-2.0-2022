c6810f07c1ace8de671811431f1e012a44e1c7da
LaDChoromota <lui.salamanca@duocuc.cl>
2022-06-24 14:12:53 -0400

    subir imagenes crud

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,10 @@
-from email.mime import image
 from django import forms
 from django.forms import ModelForm
-from .models import producto
+from.models import *
 
 class productoform(ModelForm):
     class Meta:
         model= producto
         fields =['nombre','precio','descripcion','categoria']
+        models.ImageField(upload_to="productos", null=True)
         
